1c09efa4af5b85201b8db72da08c198e6d60b15c
YusraSenimAydin <yusrasenimdede@gmail.com>
2023-12-18 23:06:00 +0700

    added slider

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,12 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Navbar from '../Navbar/NAvbar';
+import Navbar from '../Navbar/Navbar';
+import Slider from '../Slider/Slider';
 
 
 const Main = () => {
   return (
       <div>
         <Navbar />
+        <Slider />
       </div>
   );
 };
---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import slideReducer from "../features/slices/sliderSlice";
+
+
+export const store = configureStore({
+  reducer: {
+    slider: slideReducer,
+  },
+});
